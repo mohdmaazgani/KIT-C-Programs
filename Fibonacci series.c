633e162af303d838318ded8db8042caa23c31558
mohdmaazgani <232573@kit.ac.in>
2024-02-08 20:24:06 +0530

    fibonacci series using recursion

---
+++ Fibonacci series.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int num;
+    printf("Enter the number of terms: ");
+    scanf("%d", &num);
+
+    printf("Fibonacci series: ");
+    for (int i = 0; i < num; i++) {
+        printf("%d ", fibonacci(i));
+    }
+
+    return 0;
+}
