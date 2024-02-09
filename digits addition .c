4c8f8e23c9254734c14ff399d78a5c2686bc2ed6
mohdmaazgani <232573@kit.ac.in>
2024-02-09 23:11:06 +0530

    addition of digits of a number

---
+++ digits addition .c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int number, sum = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // Iterate until number becomes 0
+    while (number != 0) {
+        // Extract the last digit
+        digit = number % 10;
+        // Add the last digit to sum
+        sum += digit;
+        // Remove the last digit
+        number /= 10;
+    }
+
+    printf("Sum of the digits: %d\n", sum);
+
+    return 0;
+}
