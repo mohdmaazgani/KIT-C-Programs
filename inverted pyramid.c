04af2056e63dab376ecac503b7392fd95b488ee3
mohdmaazgani <232573@kit.ac.in>
2024-02-10 22:58:29 +0530

    full inverted pyramid

---
+++ inverted pyramid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int rows, i, j, space;
+
+    printf("Enter the number of rows for the inverted pyramid: ");
+    scanf("%d", &rows);
+
+    for (i = rows; i >= 1; i--) {
+        // Print spaces
+        for (space = 0; space < rows - i; space++)
+            printf(" ");
+
+        // Print asterisks
+        for (j = 1; j <= 2 * i - 1; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}
